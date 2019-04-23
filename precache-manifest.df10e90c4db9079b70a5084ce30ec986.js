d2be66ff0db875404871f559d78a631694376024
Ross carmack <rosscarmack@Rosss-MBP.fios-router.home>
2019-04-23 12:09:59 -0500

    Updates

---
+++ precache-manifest.df10e90c4db9079b70a5084ce30ec986.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "4155bdd61b4a9a351a940065315a0b8a",
+    "url": "/ClickyGame/static/media/jabba.4155bdd6.png"
+  },
+  {
+    "revision": "bf61aae20d32b85d0bf4",
+    "url": "/ClickyGame/static/css/main.9088a3a3.chunk.css"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/ClickyGame/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "2bc0018eeda1035de3a0df81579b0cc5",
+    "url": "/ClickyGame/static/media/ackbar.2bc0018e.png"
+  },
+  {
+    "revision": "59153367b9ff1f7d164cdacd0bce1e7a",
+    "url": "/ClickyGame/static/media/anakin.59153367.png"
+  },
+  {
+    "revision": "58a74839d454705c23dddea137676ddf",
+    "url": "/ClickyGame/static/media/bobba.58a74839.png"
+  },
+  {
+    "revision": "f7f0158ac0abf83adf4b225c4bb3ede0",
+    "url": "/ClickyGame/static/media/c3p0.f7f0158a.png"
+  },
+  {
+    "revision": "aac89741da0cb8e5a5212839d3c94f07",
+    "url": "/ClickyGame/static/media/chewie.aac89741.png"
+  },
+  {
+    "revision": "1823b4a139dc2748c75e71bf106747cc",
+    "url": "/ClickyGame/static/media/ewok.1823b4a1.png"
+  },
+  {
+    "revision": "241cd2890a741906b0456085f18081c0",
+    "url": "/ClickyGame/static/media/han.241cd289.png"
+  },
+  {
+    "revision": "bf61aae20d32b85d0bf4",
+    "url": "/ClickyGame/static/js/main.a05cbf0c.chunk.js"
+  },
+  {
+    "revision": "b64200e11aa6dd7accd9dbb7c5eab567",
+    "url": "/ClickyGame/static/media/lando.b64200e1.png"
+  },
+  {
+    "revision": "312de832eed794a624d0b87ce252aba5",
+    "url": "/ClickyGame/static/media/maul.312de832.png"
+  },
+  {
+    "revision": "8210573458066f83617a286bf547f464",
+    "url": "/ClickyGame/static/media/quigon.82105734.png"
+  },
+  {
+    "revision": "cf6560213d88c455df4dd7e5751aa0ce",
+    "url": "/ClickyGame/static/media/r2d2.cf656021.png"
+  },
+  {
+    "revision": "cb56608c26bba53426961d2b3cce3b04",
+    "url": "/ClickyGame/static/media/sebulba.cb56608c.png"
+  },
+  {
+    "revision": "e2806e8252b554f50720410188d388ea",
+    "url": "/ClickyGame/static/media/stormtrooper.e2806e82.png"
+  },
+  {
+    "revision": "0ed375b1c53d36d651b84a6894cc4ef5",
+    "url": "/ClickyGame/static/media/tusken.0ed375b1.png"
+  },
+  {
+    "revision": "890682ded57d4d538477c47539ec3379",
+    "url": "/ClickyGame/static/media/vader.890682de.png"
+  },
+  {
+    "revision": "eba8736dc2b2ba206f8000f9df6c8623",
+    "url": "/ClickyGame/static/media/yoda.eba8736d.png"
+  },
+  {
+    "revision": "88305aa8717dbb95c9ce31618d21ff40",
+    "url": "/ClickyGame/index.html"
+  }
+];
